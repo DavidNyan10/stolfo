3f109be97df64c1c1b39fa0d5b10ab296ee7d63a
vveeps <54472340+vveeps@users.noreply.github.com>
2021-10-28 18:32:45 +0300

    undo test

--- cogs/music.py
+++ cogs/music.py
@@ -83,7 +83,7 @@ class Music(Cog):
             player = self.bot.pomice.get_node().get_player(guild.id)
 
         if not after.channel:
-            return
+            return await player.destroy()
 
         if player.is_playing and before.channel != after.channel:
             paused = player.is_paused
