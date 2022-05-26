c005ec3ddc1a81b160b028db05160dc67a8aed32
vveeps <54472340+vveeps@users.noreply.github.com>
2022-05-26 16:41:47 +0300

    nvm need to fix somehow else

--- tracks.py
+++ tracks.py
@@ -86,7 +86,7 @@ class YouTubeTrack(SearchableTrack, _YoutubeTrack):
 class YouTubePlaylist(_YouTubePlaylist, SearchableTrack):
     _search_type: ClassVar[str] = "ytpl"
 
-    def __init__(self, data: dict):
+    def __init__(self, data: dict, ctx: Context):
         self.tracks: list[YouTubeTrack] = []
         self.name: str = data["playlistInfo"]["name"]
 
